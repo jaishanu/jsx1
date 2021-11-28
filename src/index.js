355d82d76c8c42adf242bd68c0ba49b2f4ecb722
jaishanu <91627941+jaishanu@users.noreply.github.com>
2021-11-28 21:34:08 +0530

    list creation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
+import React from "react";
+import reactdom from "react-dom";
+reactdom.render(
+  <div>
+    <h1>List1</h1>
+    <ul>
+      <li>first</li>
+      <li>second</li>
+      <li>third</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
